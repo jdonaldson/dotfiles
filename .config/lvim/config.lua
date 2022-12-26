9da698e33c3306fd7b990ec0ddea94560b3b7247
Justin Donaldson <jdonaldson@gmail.com>
2022-12-26 11:10:19 -0600

    add util code for lvim

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -7,6 +7,7 @@ a global executable or a path to
 an executable
 ]]
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+local util = require("util")
 
 -- general
 lvim.log.level = "warn"
